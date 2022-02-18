b5bda2db1726dbc3efe9505c905596ed519549c4
unknown <cristian.daniel.herrera@hotmail.com>
2022-02-18 16:01:54 +0000

    firstCommit

--- src/components/Letters/LettersStyles.js
+++ src/components/Letters/LettersStyles.js
@@ -1,8 +1,8 @@
 import styled from "styled-components";
 
 export const LettersContainer = styled.div`
-width: 30%;
-height: 15%;
+width: 45%;
+height: 35%;
 background-color: #0000003d;
 border-radius: 20px;
 padding: 30px;
@@ -11,5 +11,20 @@ color: white;
 position: absolute;
 top: 40%;
 left: 10%;
+
+
+
+
+@keyframes taadaa2 { 
+    0% {
+      left: -100%;
+    }
+  
+    100% {
+      left: 10%;
+    }
+  }
+
+  animation: taadaa2   1s cubic-bezier(.92,.29,.65,.59) 1;
 
 `;

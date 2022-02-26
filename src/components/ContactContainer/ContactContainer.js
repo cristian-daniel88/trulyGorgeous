2de5a7e58b6e0d06c4a662302c1785699cb9d287
unknown <cristian.daniel.herrera@hotmail.com>
2022-02-26 22:51:18 +0000

    firstCommit

--- src/components/ContactContainer/ContactContainer.js
+++ src/components/ContactContainer/ContactContainer.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { ContactBoxes1, ContactBoxes2, ContactDetails, ContactDetailsContainer, ContactForm, ContainerBoxesContact, ContainerContact, Form, IframeBox } from './ContactContainerStyles'
+import { BoldContact, ContactBoxes1, ContactBoxes2, ContactDetails, ContactDetailsContainer, ContactForm, ContainerBoxesContact, ContainerContact, AdressContainer, Form, IframeBox, PContact, PhoneContainer, IframeBoxContainer } from './ContactContainerStyles'
 
 function ContactContainer() {
   return (
@@ -8,21 +8,42 @@ function ContactContainer() {
             <ContactBoxes1>
                 <ContactDetails>
                     <ContactDetailsContainer>
+                            <PhoneContainer>
+                                <PContact><BoldContact>Phone: </BoldContact>01243 788090</PContact>
+                                <PContact><BoldContact>Email: </BoldContact>mail@trulygorgeous.co.uk</PContact>
+                            </PhoneContainer>
 
+
+                            <AdressContainer>
+                            <PContact><BoldContact>Adress: </BoldContact>Truly Gorgeous Unit 4 (South) Vinnetrow Business Park Vinnetrow Road Chichester – West Sussex PO20 1QH</PContact>
+                            </AdressContainer>
                     </ContactDetailsContainer>
                 </ContactDetails>
+
+
                 <ContactForm>
                     <Form>
-
+                        Form
                     </Form>
                 </ContactForm>
+
+
             </ContactBoxes1>
+
+
+
+
             <ContactBoxes2>
+              <IframeBoxContainer>
+                  
+
 
                 <IframeBox  src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d10081.628828875313!2d-0.7505814!3d50.8236209!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xcf93b380bcdb6e6e!2sTruly%20Gorgeous%20Wedding%20Dresses!5e0!3m2!1ses-419!2suk!4v1645437678011!5m2!1ses-419!2suk"
           allowFullScreen="" loading="lazy">
 
                 </IframeBox>
+              
+              </IframeBoxContainer>
             </ContactBoxes2>
         </ContainerBoxesContact>
     </ContainerContact>
--- src/components/ContactContainer/ContactContainerStyles.js
+++ src/components/ContactContainer/ContactContainerStyles.js
@@ -10,9 +10,10 @@ padding: 2% 10%;
 
 export const ContainerBoxesContact = styled.div`
 width: 100%;
-height: 100%;
+height: auto;
 //border: solid 1px black;
 display: flex;
+flex-wrap: wrap;
 
 `
 
@@ -20,11 +21,18 @@ export const ContactBoxes1 = styled.div`
 width: 45%;
 height: 100%;
 //border: solid 1px green;
+min-width: 300px;
+
+@media screen and (max-width: 865px) {
+   width: 100%;
+  
+   
+  }
 `
 
 export const ContactDetails = styled.div`
 width: 100%;
-height: 30%;
+height: 20%;
 //background-color: green;
 padding: 10px;
 `
@@ -33,9 +41,60 @@ padding: 10px;
 export const ContactDetailsContainer = styled.div`
 width: 100%;
 border-radius: 10px;
-height: 100%;
+height: auto;
 border: solid 1px #e8d8d8;
 background-color: white;
+display: flex;
+flex-wrap: wrap;
+justify-content: center;
+
+padding: 1px;
+`
+export const PhoneContainer = styled.div`
+width: 50%;
+height: auto;
+//background-color: blue;
+min-width: 200px;
+@import url('https://fonts.googleapis.com/css2?family=Alef&family=PT+Sans&family=Raleway:ital@1&display=swap');
+font-family: 'Alef', sans-serif;
+font-family: 'Raleway', sans-serif;
+font-family: 'PT Sans', sans-serif;
+font-size: 13px;
+color: #747474;
+padding-top: 10px;
+padding-bottom: 10px;
+padding-left: 2px;
+@media screen and (max-width: 1200px) {
+    width: 100%;
+  }
+`
+export const AdressContainer = styled.div`
+width: 50%;
+height: auto;
+//background-color: black;
+min-width: 200px;
+padding-top: 5px;
+@import url('https://fonts.googleapis.com/css2?family=Alef&family=PT+Sans&family=Raleway:ital@1&display=swap');
+font-family: 'Alef', sans-serif;
+font-family: 'Raleway', sans-serif;
+font-family: 'PT Sans', sans-serif;
+font-size: 13px;
+color: #747474;
+
+@media screen and (max-width: 1200px) {
+    width: 100%;
+  }
+
+`
+
+export const BoldContact = styled.strong`
+
+
+
+`
+export const PContact = styled.p`
+font-size: 12px;
+
 `
 
 
@@ -72,20 +131,36 @@ background-color: white;
 
 export const ContactBoxes2 = styled.div`
 width: 55%;
-height: 100%;
+height: auto;
 
 display: flex;
 justify-content: center;
 
 
+@media screen and (max-width: 865px) {
+   width: 100%;
+  
+   
+  }
 
+
+
+`
+
+export const IframeBoxContainer = styled.div`
+width: 100%;
+display: flex;
+justify-content: center;
+//background-color: white;
+align-items: center;
+padding-bottom: 10px;
 `
 
 export const IframeBox = styled.iframe`
 border: none;
-height: 65%;
+height: 400px;
 width: 90%;
-margin-top: 20px;
+margin-top: 10px;
 
 
 `

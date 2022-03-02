15720b0a506d0f8187d0665af40fc7a97b823ebe
unknown <cristian.daniel.herrera@hotmail.com>
2022-03-02 20:42:45 +0000

    firstCommit

--- src/database/designers.js
+++ src/database/designers.js
@@ -33,13 +33,44 @@ export const designers = [
             { 
                 uid: '69404',   
                 name: '', 
-                imgs: [],
+                imgs: [
+
+                  'https://res.cloudinary.com/truly-gorgeous/image/upload/v1646251797/designers/ronaldJoyce/69404/69404_109_fzpixn.jpg',
+
+                  'https://res.cloudinary.com/truly-gorgeous/image/upload/v1646251797/designers/ronaldJoyce/69404/69404_4189_mkl5os.jpg',
+
+                  'https://res.cloudinary.com/truly-gorgeous/image/upload/v1646251797/designers/ronaldJoyce/69404/69404_050_d1mdye.jpg',
+
+                  'https://res.cloudinary.com/truly-gorgeous/image/upload/v1646251797/designers/ronaldJoyce/69404/69404_051_xsjurj.jpg',
+
+                  'https://res.cloudinary.com/truly-gorgeous/image/upload/v1646251797/designers/ronaldJoyce/69404/69404_4047_hkuvjz.jpg',
+
+                  'https://res.cloudinary.com/truly-gorgeous/image/upload/v1646251797/designers/ronaldJoyce/69404/69404_091_cclomq.jpg',
+
+                  'https://res.cloudinary.com/truly-gorgeous/image/upload/v1646251796/designers/ronaldJoyce/69404/69404_3816_wqx2fg.jpg',
+
+                  'https://res.cloudinary.com/truly-gorgeous/image/upload/v1646251795/designers/ronaldJoyce/69404/69404_006_jrrh5s.jpg'
+                ],
             },
 
             { 
                 uid: '69153',   
                 name: '', 
-                imgs: [],
+                imgs: [
+
+                 'https://res.cloudinary.com/truly-gorgeous/image/upload/v1646251795/designers/ronaldJoyce/69153/69153-0164_wqfyta.jpg',
+                 
+                 'https://res.cloudinary.com/truly-gorgeous/image/upload/v1646251795/designers/ronaldJoyce/69153/69153_03_002_nz5xyn.jpg',
+
+                 'https://res.cloudinary.com/truly-gorgeous/image/upload/v1646251795/designers/ronaldJoyce/69153/69153-0164R_gsy1kl.jpg',
+
+                 'https://res.cloudinary.com/truly-gorgeous/image/upload/v1646251795/designers/ronaldJoyce/69153/69153-0143R_z7bmoc.jpg',
+
+                 'https://res.cloudinary.com/truly-gorgeous/image/upload/v1646251793/designers/ronaldJoyce/69153/69153-0049R_p5g8ig.jpg',
+
+                 'https://res.cloudinary.com/truly-gorgeous/image/upload/v1646251793/designers/ronaldJoyce/69153/69153-0049_cmztfu.jpg'
+
+                ],
             },
             { 
                 uid: '69712',   
